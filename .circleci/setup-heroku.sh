202be4b5ecba8aa49330ae64e776e146b5fcbed4
Zachary Scott <mail@zzak.io>
2017-06-14 14:36:26 +0900

    Extract Heroku setup to script

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,11 @@
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+heroku git:remote -a $HEROKU_APP
